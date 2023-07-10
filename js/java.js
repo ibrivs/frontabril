56645c16ae4722704573e446b2fe2ff5042dc420
ibrivs <134823948+ibrivs@users.noreply.github.com>
2023-07-10 00:10:52 -0300

    Add files via upload

---
+++ js/java.js
@@ -0,0 +1,21 @@
+alert("Bienvenido, encontraras una sencilla, rapida y deliciosa receta para cenar o almorzar como se debe!")
+function noche() {
+  document.body.style.backgroundColor= "black" ;
+  document.getElementById("nav").style.backgroundColor = "gray";
+  document.getElementById("hache").style.color = "black";
+  document.getElementById("pie").style.backgroundColor = "Gray" ;
+  document.getElementById("recet").style.color = "white" ;
+  document.getElementById("ingr").style.color = "white";
+  document.getElementById("paso").style.color= "white" ;
+  document.getElementById("lista").style.color= "white" ;
+  document.getElementById("papa").style.color= "white" ;
+  document.getElementById("papa").style.color= "white" ;
+  document.getElementById("torti").style.color= "white" ;
+  document.getElementById("pau").style.color= "white" ;
+  document.getElementById("papa").style.color= "white" ;
+  document.getElementById("logo").style.backgroundColor= "gray" ;
+}
+
+function autoria() {
+    alert("Hecho por: Abril Varas para FrontEndSM");
+    }
